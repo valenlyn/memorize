aa8b9f94f63a35b47db594425a94834d4d9cb8c7
valenlyn <valenlynchua@gmail.com>
2022-05-30 23:12:14 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Valenlyn Chua on 30/5/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
